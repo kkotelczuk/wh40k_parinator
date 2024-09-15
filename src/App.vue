7cffabd71c2634342dca5f89155d518e03a294cb
Konrad Kotelczuk <kkotelczuk@users.noreply.github.com>
2024-09-15 17:55:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import './assets/index.css'
+</script>
+
+<template>
+  <div>
+    <Button>
+      Click me
+    </Button>
+  </div>
+</template>
